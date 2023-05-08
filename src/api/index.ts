463b298e53b916f616f1bbcb153df8d140643c32
Андрей <andrey.alekseevich.22@yandex.ru>
2023-05-08 11:48:34 +0300

    env

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,13 @@ import axios from "axios";
 const JWT_TOKEN = localStorage.getItem("token") || "";
 
 const API_URL = process.env.REACT_APP_API_URL;
+const ORIGIN = process.env.REACT_APP_ORIGIN;
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
 
   headers: {
-    "Access-Control-Allow-Origin": "*:*",
+    "Access-Control-Allow-Origin": ORIGIN,
     Authorization: `Bearer ${JWT_TOKEN}`,
   },
 });
