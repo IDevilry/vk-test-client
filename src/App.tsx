d926fa15ad84439f2a8f1a55132f038a99a2b426
IDevilry <andrey.alekseevich.22@yandex.ru>
2023-05-08 09:43:57 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { useEffect, type FC } from "react";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import {
-  Feed,
-  Friends,
-  Profile,
-  SignIn,
-  SignUp,
-  Chat,
-  UserProfile,
-  UsersList,
+	Feed,
+	Friends,
+	Profile,
+	SignIn,
+	SignUp,
+	Chat,
+	UserProfile,
+	UsersList,
 } from "./pages";
 
 import { PrivateWrapper, Layout } from "./components";
@@ -22,95 +22,97 @@ import { fetchChats } from "./redux/asyncThunks/chat/fetchChats";
 import { socket } from "./socket";
 
 const App: FC = () => {
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
-  const user = useAppSelector((state) => state.currentUser.user);
+	const user = useAppSelector((state) => state.currentUser.user);
 
-  useEffect(() => {
-    socket.emit("addNewUser", user);
+	useEffect(() => {
+		socket.emit("addNewUser", user);
 
-    return () => {
-      socket.emit("userDisconnected", user);
-    };
-  }, []);
+		return () => {
+			socket.emit("userDisconnected", user);
+		};
+	}, []);
 
-  useEffect(() => {
-    dispatch(fetchCurrentUser());
-    dispatch(fetchFriends());
-    dispatch(fetchChats({ userId: user?._id }));
-  }, []);
+	useEffect(() => {
+		if (user) {
+			dispatch(fetchCurrentUser());
+			dispatch(fetchFriends());
+			dispatch(fetchChats({ userId: user?._id }));
+		}
+	}, []);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth/">
-          <Route path="register" element={<SignUp />} />
-          <Route path="login" element={<SignIn />} />
-        </Route>
-        <Route
-          path="/"
-          element={
-            <PrivateWrapper>
-              <Layout />
-            </PrivateWrapper>
-          }
-        >
-          <Route
-            index
-            element={
-              <PrivateWrapper>
-                <Feed />
-              </PrivateWrapper>
-            }
-          />
-          <Route
-            path="profile"
-            element={
-              <PrivateWrapper>
-                <Profile />
-              </PrivateWrapper>
-            }
-          />
-          <Route
-            path="profile/:id"
-            element={
-              <PrivateWrapper>
-                <UserProfile />
-              </PrivateWrapper>
-            }
-          />
-          <Route
-            path="chat"
-            element={
-              <PrivateWrapper>
-                <Chat />
-              </PrivateWrapper>
-            }
-          />
-          <Route path="friends/">
-            <Route
-              path="my"
-              element={
-                <PrivateWrapper>
-                  <Friends />
-                </PrivateWrapper>
-              }
-            />
-            <Route
-              path="search"
-              element={
-                <PrivateWrapper>
-                  <UsersList />
-                </PrivateWrapper>
-              }
-            />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/auth/">
+					<Route path="register" element={<SignUp />} />
+					<Route path="login" element={<SignIn />} />
+				</Route>
+				<Route
+					path="/"
+					element={
+						<PrivateWrapper>
+							<Layout />
+						</PrivateWrapper>
+					}
+				>
+					<Route
+						index
+						element={
+							<PrivateWrapper>
+								<Feed />
+							</PrivateWrapper>
+						}
+					/>
+					<Route
+						path="profile"
+						element={
+							<PrivateWrapper>
+								<Profile />
+							</PrivateWrapper>
+						}
+					/>
+					<Route
+						path="profile/:id"
+						element={
+							<PrivateWrapper>
+								<UserProfile />
+							</PrivateWrapper>
+						}
+					/>
+					<Route
+						path="chat"
+						element={
+							<PrivateWrapper>
+								<Chat />
+							</PrivateWrapper>
+						}
+					/>
+					<Route path="friends/">
+						<Route
+							path="my"
+							element={
+								<PrivateWrapper>
+									<Friends />
+								</PrivateWrapper>
+							}
+						/>
+						<Route
+							path="search"
+							element={
+								<PrivateWrapper>
+									<UsersList />
+								</PrivateWrapper>
+							}
+						/>
 
-            <Route />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+						<Route />
+					</Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- src/redux/slices/user/friendsSlice.ts
+++ src/redux/slices/user/friendsSlice.ts
@@ -5,34 +5,34 @@ import { toggleFriend } from "../../asyncThunks";
 import { type IUser } from "../../../types";
 
 interface FriendsState {
-  friends: IUser[];
-  totalCount: number;
+	friends: IUser[];
+	totalCount: number;
 }
 
 const initialState: FriendsState = {
-  friends: [],
-  totalCount: 0,
+	friends: [],
+	totalCount: 0,
 };
 
 const friendsSlice = createSlice({
-  name: "friends",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchFriends.fulfilled, (state, action) => {
-      state.friends = action.payload.user.friends;
-      state.totalCount = action.payload.totalCount;
-    });
-    builder.addCase(toggleFriend.fulfilled, (state, action) => {
-      state.friends.find(
-        (friend) => friend._id === action.payload.targetUser._id
-      )
-        ? (state.friends = state.friends.filter(
-            (friend) => friend._id !== action.payload.targetUser._id
-          ))
-        : state.friends.unshift(action.payload.targetUser);
-    });
-  },
+	name: "friends",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(fetchFriends.fulfilled, (state, action) => {
+			state.friends = action.payload?.user?.friends;
+			state.totalCount = action.payload.totalCount;
+		});
+		builder.addCase(toggleFriend.fulfilled, (state, action) => {
+			state.friends.find(
+				(friend) => friend._id === action.payload.targetUser._id
+			)
+				? (state.friends = state.friends.filter(
+						(friend) => friend._id !== action.payload.targetUser._id
+				  ))
+				: state.friends.unshift(action.payload.targetUser);
+		});
+	},
 });
 
 export default friendsSlice.reducer;
