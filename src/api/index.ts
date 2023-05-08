8b70d7883f6d10dac791678ef17a009de4e0dbbf
Андрей <andrey.alekseevich.22@yandex.ru>
2023-05-08 13:46:42 +0300

    axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,13 +3,15 @@ import axios from "axios";
 const JWT_TOKEN = localStorage.getItem("token") || "";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const ORIGIN = process.env.REACT_APP_ORIGIN;
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
 
   headers: {
-    "Access-Control-Allow-Origin": ORIGIN,
     Authorization: `Bearer ${JWT_TOKEN}`,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
   },
 });
--- src/pages/Friends/Friends.tsx
+++ src/pages/Friends/Friends.tsx
@@ -1,21 +1,27 @@
-import { useState, type FC, type ChangeEventHandler } from "react";
-import { useAppSelector } from "../../hooks/typedRedux";
+import { useState, type FC, type ChangeEventHandler, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/typedRedux";
 
 import { User } from "../../components";
 
 import cn from "./friends.module.css";
+import { fetchFriends } from "../../redux/asyncThunks";
 
 const Friends: FC = () => {
   const [search, setSearch] = useState("");
 
   const friends = useAppSelector((state) => state.friends.friends);
   const currentUser = useAppSelector((state) => state.currentUser.user);
+  const dispatch = useAppDispatch();
 
   const filtered = friends.filter(
     (user) =>
       user.user_first_name.toLowerCase().includes(search.toLowerCase()) ||
       user.user_last_name.toLowerCase().includes(search.toLowerCase())
   );
+
+  useEffect(() => {
+    dispatch(fetchFriends());
+  }, [dispatch]);
 
   const handleSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
     setSearch(e.target.value);
